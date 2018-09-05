058d129d980635e6994e2637b191d0069ade112c
bob <bob@APCSA>
2018-09-05 14:42:52 -0700

    Local commit of TwoIntegers.

--- src/Twointegers.java
+++ src/Twointegers.java
@@ -1,4 +1,22 @@
+public class Twointegers 
+{
+	private int num1;
+	private int num2;
+	
+	public Twointegers()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	
+	public Twointegers(int num1, int num2)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public String arithmetic()
+	{	
 
-public class Twointegers {
-
+	}
 }
